40bb926e506d9df6d600a7fc32efe727259cdb82
GabrielBndo <gabriel.bernardo.cruuz@gmail.com>
2022-12-14 19:20:05 -0300

    adição do post

--- CineNac/vite-project/src/components/BaseTable.jsx
+++ CineNac/vite-project/src/components/BaseTable.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css'
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -9,7 +10,9 @@ export default function BaseTable() {
     const [rows, setRows] = useState([]);
     const [nome, setNome] = useState([]);
     const [ano, setAno] = useState([]);
-    const [classs, setClasss] = useState([]);
+    const [idade, setIdade] = useState([]);
+    const [id, setId] = useState([]);
+    const apiEndPoint = 'http://localhost:3002/filmes';
 
   useEffect(() => {
     axios
@@ -24,35 +27,66 @@ export default function BaseTable() {
     const inputAno = (e) => {
       setAno(e.target.value);
     };
-    const inputClasss = (e) => {
-      setClasss(e.target.value);
+    const inputIdade = (e) => {
+      setIdade(e.target.value);
     };
     function postNome()
     {
-        axios.post('http://localhost:3002/filmes', { nome: nome, ano: ano, classificacao: classs })
+        axios.post('http://localhost:3002/filmes', { nome: nome, ano: ano, idade: idade })
         .then(axios.get("http://localhost:3002/filmes"))
         .then((Response) => setRows(Response.data))
         .then((error) => console.log(error));
     }
+    
+    //-------------Atualizar----------------------------------------------------
+
+    const inputName = (e) => {
+      setNome(e.target.value)
+  }
+  const inputYear = (e) => {
+    setAno(e.target.value);
+  };
+  const inputAge = (e) => {
+    setIdade(e.target.value);
+  };
+  function postNome()
+  {
+      axios.post('http://localhost:3002/filmes', { nome: nome, ano: ano, idade: idade })
+      .then(axios.get("http://localhost:3002/filmes"))
+      .then((Response) => setRows(Response.data))
+      .then((error) => console.log(error));
+  }
+    
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <header>
-            <label>Nome</label>
+            <label>Título</label>
             <input onChange={(e) => inputNome(e)} /> 
-            <label>E-mail</label>
+            <label>Ano</label>
             <input onChange={(e) => inputAno(e)} /> 
             <label>Idade</label>
-            <input onChange={(e) => inputClasss(e)} /> 
+            <input onChange={(e) => inputIdade(e)} /> 
             <Button variant="primary" onClick={() => postNome()}>Adicionar</Button>{" "}
+            
+          </header><header>
+            <label>ID</label>
+            <input onChange={(e) => inputNome(e)} />
+            <label>Título</label>
+            <input onChange={(e) => inputNome(e)} /> 
+            <label>Ano</label>
+            <input onChange={(e) => inputAno(e)} /> 
+            <label>Idade</label>
+            <input onChange={(e) => inputIdade(e)} /> 
+            <Button variant="primary" onClick={() => postNome()}>Update</Button>{" "}
           </header>
         </tr>
         <tr>
           <th>ID</th>
           <th>Nome</th>
           <th>Ano</th>
-          <th>Classificacao</th>
+          <th>Idade</th>
           <th>Escolha</th>
         </tr>
       </thead>
@@ -61,10 +95,10 @@ export default function BaseTable() {
           <tr Key={rows.id}>
             <td>{row.id}</td>
             <td>{row.nome}</td>
-            <td>{row.email}</td>
+            <td>{row.ano}</td>
             <td>{row.idade}</td>
             <td>
-              <Button variant="primary">Editar</Button>{" "}
+              <Button variant="primary" onClick={() => handleUpdate(post)}>Editar</Button>{" "}
               <Button variant="danger">Excluir</Button>{" "}
             </td>
           </tr>
